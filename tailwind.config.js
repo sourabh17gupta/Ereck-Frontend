e94b1d2c0a1b780b32a668fccbe45ca761d4d6fe
Sourabh Gupta <123104060@nitkkr.ac.in>
2025-11-06 03:56:25 +0530

    gallery making

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,15 @@ module.exports = {
         yellow: {
           50: "#FFD60A",
         },
+        keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.8s ease-in forwards",
+      },
       },
     },
   },
