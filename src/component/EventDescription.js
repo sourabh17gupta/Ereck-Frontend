6d3b5f8b2b12b91b47db4affaad567746e241203
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 01:10:03 +0530

    Update EventDescription.js

--- src/component/EventDescription.js
+++ src/component/EventDescription.js
@@ -78,7 +78,7 @@ const EventDescription = () => {
           onClick={() => navigate(-1)}
           className="bg-yellow-500 text-black px-4 py-2 rounded-lg font-medium hover:bg-yellow-400 transition-all"
         >
-          ← Back
+          Back
         </button>
       </div>
 
