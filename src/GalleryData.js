1d30c7b3c2e5046052f7631a2b9d1909e8290cbb
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 01:49:30 +0530

    Update GalleryData.js

--- src/GalleryData.js
+++ src/GalleryData.js
@@ -1,23 +1,23 @@
 export const galleryData = {
-  "Tech Fest 2024": [
+  "Tech-Spardha": [
     { id: 1, src: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b", caption: "Robotics Challenge" },
     { id: 2, src: "https://images.unsplash.com/photo-1519389950473-47ba0277781c", caption: "Hackathon Winners" },
     { id: 3, src: "https://images.unsplash.com/photo-1581090700227-1e37b190418e", caption: "AR/VR Showcase" },
     { id: 4, src: "https://images.unsplash.com/photo-1518770660439-4636190af475", caption: "Coding Arena" },
   ],
-  "Sports Meet 2024": [
+  "Our-Senior": [
     { id: 1, src: "https://images.unsplash.com/photo-1502877338535-766e1452684a", caption: "Football Finals" },
     { id: 2, src: "https://images.unsplash.com/photo-1508264165352-258a6ec93d8e", caption: "Cricket Match" },
     { id: 3, src: "https://images.unsplash.com/photo-1574629810360-7efbbe195018", caption: "Track Events" },
     { id: 4, src: "https://images.unsplash.com/photo-1521412644187-c49fa049e84d", caption: "Medal Ceremony" },
   ],
-  "Cultural Night 2024": [
+  "WorkShops": [
     { id: 1, src: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91", caption: "Dance Performance" },
     { id: 2, src: "https://images.unsplash.com/photo-1502786129293-79981df4e689", caption: "Drama Act" },
     { id: 3, src: "https://images.unsplash.com/photo-1507874457470-272b3c8d8ee2", caption: "Live Music" },
     { id: 4, src: "https://images.unsplash.com/photo-1521334884684-d80222895322", caption: "Fashion Show" },
   ],
-  "Annual Meetup 2024": [
+  "Group-Photos": [
     { id: 1, src: "https://images.unsplash.com/photo-1494790108377-be9c29b29330", caption: "Team Gathering" },
     { id: 2, src: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d", caption: "Guest Speaker Session" },
     { id: 3, src: "https://images.unsplash.com/photo-1584697964403-5e89d8f3aaf3", caption: "Award Ceremony" },
