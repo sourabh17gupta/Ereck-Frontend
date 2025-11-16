5576468cd6e3ffd0ea2617fc87fe9cb0f67c619a
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 15:08:08 +0530

    Update teamDetail.js

--- src/api/Services/teamDetail.js
+++ src/api/Services/teamDetail.js
@@ -7,9 +7,7 @@ const { TeamDetail_API } = teamDetailsPoint;
 // API call to fetch single team
 export const getSingleTeam = async (teamName) => {
   try {
-    console.log("Fetching team:", teamName);
     const response = await apiConnector("GET", `${TeamDetail_API}/${teamName}`);
-    console.log(response);
     return response?.data?.data || null;
   } catch (error) {
     console.error(`Error fetching team ${teamName}:`, error);
